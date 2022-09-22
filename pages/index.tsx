309b85b589cbdbe06054799bee2b9fd377bdc449
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-09-22 13:36:40 +0200

    pageinfo correction

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,7 +86,7 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
 export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo[] = await fetchPageInfo();
+  const pageInfo: PageInfo = await fetchPageInfo();
   const experiences: Experience[] = await fetchExperiences();
   const skills: Skill[] = await fetchSkills();
   const projects: Project[] = await fetchProjects();
